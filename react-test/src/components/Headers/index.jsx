f8b0747aae64342b15231af9900e46719b27d9bb
fauzanrozzaqo <rizkianakbar4@gmail.com>
2021-08-11 11:19:38 +0700

    📦 NEW : header with styled-components

---
+++ react-test/src/components/Headers/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Title, Search } from "./styled";
+
+const Header = () => {
+  return (
+    <>
+      <Container>
+        <Title>Movie App</Title>
+      </Container>
+    </>
+  );
+};
+
+export default Header;
